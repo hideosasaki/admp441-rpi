64e1b61a10860415b3b5a1ca0efe2d6e2d7a703e
SASAKI Hideo <sasaki@rio.ne.jp>
2021-09-15 17:18:17 +0900

    Update admp441.c

--- kernel/admp441.c
+++ kernel/admp441.c
@@ -26,7 +26,7 @@ static struct snd_soc_dai_driver admp441_dai = {
 	},
 };
 
-static struct snd_soc_dai admp441_soc_driver = { };
+// static struct snd_soc_dai admp441_soc_driver = { };
 
 static struct snd_soc_component_driver admp441_soc_component = {
 	.idle_bias_on		= 1,
@@ -37,13 +37,12 @@ static struct snd_soc_component_driver admp441_soc_component = {
 
 static int admp441_probe(struct platform_device *pdev)
 {
-	admp441_soc_driver = *devm_snd_soc_register_component(&pdev->dev, &admp441_soc_component, &admp441_dai, 1);
-	return 0;
+	return devm_snd_soc_register_component(&pdev->dev, &admp441_soc_component, &admp441_dai, 1);
 }
 
 static int admp441_remove(struct platform_device *pdev)
 {
-	snd_soc_unregister_component(&admp441_soc_driver);
+	snd_soc_unregister_component(&pdev->dev);
 	return 0;
 }
 
