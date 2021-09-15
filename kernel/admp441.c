6ccf69da1acff208e95e05eec2326318ce7936d4
SASAKI Hideo <sasaki@rio.ne.jp>
2021-09-15 13:03:36 +0900

    Update admp441.c

--- kernel/admp441.c
+++ kernel/admp441.c
@@ -29,7 +29,7 @@ static struct snd_soc_dai_driver admp441_dai = {
 static struct snd_soc_component admp441_component = {
 	.name = "admp441-hifi",
 	.id = 0,
-	.name_prefix = "admp441"
+	.name_prefix = "admp441",
 	.dev = {},
 	.card = {}
 };
